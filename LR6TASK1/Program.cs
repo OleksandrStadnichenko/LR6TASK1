476e68dcf023479639fdd9d15bb13dbbe8d62629
OleksandrStadnichenko <hshdsaja@gmail.com>
2026-05-24 22:17:05 +0000

    Добавьте файлы проекта.

---
+++ LR6TASK1/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Random o = new Random();
+        List<int> chisla = new List<int>();
+        int N = 15;
+        for (int i = 0; i < N; i++)
+        {
+            chisla.Add(o.Next(5, 26));
+            Console.Write("{0} ", chisla[i]);
+        }
+        Console.WriteLine();
+
+        for (int i = chisla.Count - 1; i >= 0; i--)
+        {
+            if (chisla[i] % 10 == 1)
+            {
+                chisla.RemoveAt(i);
+                break;
+            }
+        }
+
+        for (int i = 0; i < chisla.Count; i++)
+        {
+            Console.Write("{0} ", chisla[i]);
+        }
+    }
+}
+
+
+
